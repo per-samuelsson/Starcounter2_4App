fdaf52b57e6f16dd9e3ea2886eabadedcc8dd0d2
Per Samuelsson <per@starcounter.com>
2018-10-18 11:51:04 +0200

    Added sources

---
+++ src/App/Program.cs
@@ -0,0 +1,18 @@
+﻿using Starcounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    [Database] public class Foo { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
